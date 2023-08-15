099a7567d9f6b1d825496058b77f8b1fe9d4111e
mithulix <mohaiminul.devs@gmail.com>
2023-08-15 18:21:58 +0600

    done

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -4,7 +4,7 @@ import path from 'path';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
-  node_env: process.env.NODE_ENV,
+  env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   default_student_pass: process.env.DEFAULT_STUDENT_PASS,
